dcf62f27f7d9c18e9d2ab19c23c33e91ffbb35ca
tupv <tu.pv2200@gmail.com>
2023-07-11 00:33:27 +0700

    setup vite + eslint + prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,24 +4,41 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:react-hooks/recommended",
+    "eslint-config-prettier",
+    "prettier",
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
     project: true,
     tsconfigRootDir: __dirname,
   },
-  plugins: ['react-refresh'],
+  plugins: ["react-refresh", "prettier"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
-    '@typescript-eslint/no-non-null-assertion': 'off',
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "prettier/prettier": [
+      "warn",
+      {
+        arrowParens: "always",
+        semi: false,
+        trailingComma: "none",
+        tabWidth: 2,
+        endOfLine: "auto",
+        useTabs: false,
+        singleQuote: true,
+        printWidth: 120,
+        jsxSingleQuote: true,
+      },
+    ],
   },
-}
+  ignorePatterns: [".eslintrc.cjs", "vite.config.ts"],
+};
